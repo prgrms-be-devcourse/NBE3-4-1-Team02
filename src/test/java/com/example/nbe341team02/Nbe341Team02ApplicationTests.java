426d6312b3309e15776d0fe5b7f718a4174cb223
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-12 22:59:15 +0900

    init commit

---
+++ src/test/java/com/example/nbe341team02/Nbe341Team02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbe341team02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe341Team02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
