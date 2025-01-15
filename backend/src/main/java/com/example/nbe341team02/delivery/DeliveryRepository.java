5bd971e36d6b9b98632e9629c6a26465b29d54e1
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-15 14:31:24 +0900

    feat- 배송/택배사 레포지터리 추가, 배송 서비스 클래스 추가

---
+++ backend/src/main/java/com/example/nbe341team02/delivery/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package com.example.nbe341team02.delivery;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+}
