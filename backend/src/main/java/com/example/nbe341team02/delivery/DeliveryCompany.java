75d9cbe233e3ac79f2e42ed07c2bee9e9986f41c
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-15 12:43:59 +0900

    feat- 배송 관련 엔티티 클래스(배송, 택배사) 생성

---
+++ backend/src/main/java/com/example/nbe341team02/delivery/DeliveryCompany.java
@@ -0,0 +1,19 @@
+package com.example.nbe341team02.delivery;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DeliveryCompany {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    private String companyName;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
+    private String trackingURLTemplate;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+}
