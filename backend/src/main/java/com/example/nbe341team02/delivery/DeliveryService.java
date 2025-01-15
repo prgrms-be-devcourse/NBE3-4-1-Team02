c51f0d391081dfbcc83710f2f017107c902868cf
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-15 16:43:10 +0900

    feat- 특정 시간 사이에 받은 주문을 배송처리하는 비즈니스 로직 추가, 스케쥴러를 서비스 말고 별도로 분리 (배송 시작 시간을 변경 가능하게 하기 위해)

--- backend/src/main/java/com/example/nbe341team02/delivery/DeliveryService.java
+++ backend/src/main/java/com/example/nbe341team02/delivery/DeliveryService.java
@@ -1,12 +1,45 @@
 package com.example.nbe341team02.delivery;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DeliveryService {
     private final OrderRepository orderRepository;
     private final DeliveryRepository deliveryRepository;
     private final DeliveryCompanyRepository deliveryCompanyRepository;
+
+    public void startDelivery(LocalDateTime start, LocalDateTime end){
+        List<Set<Order>> getOrdersToBeDelivered = getOrdersToBeDelivered(start, end);
+        Set<DeliveryCompany> activeDeliveryCompanies = deliveryCompanyRepository.findByActiveIs(true);
+        for (Set<Order> orders : getOrdersToBeDelivered) {
+            saveDelivery(orders, activeDeliveryCompanies);
+        }
+    }
+
+    public void setDeliveryTrackingNumber(Long deliveryId, String trackingNumber){
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow();
+        delivery.setDeliveryTrackingNumber(trackingNumber);
+    }
+
+    private void saveDelivery(Set<Order> ordersToBeDelivered, Set<DeliveryCompany> activeDeliveryCompanies){
+        deliveryRepository.save(Delivery.builder()
+                .deliveryCompany(activeDeliveryCompanies.stream().findAny().orElseThrow())
+                .orders(ordersToBeDelivered)
+                .build());
+    }
+
+    private Set<Set<Order>> getOrdersToBeDelivered(LocalDateTime start, LocalDateTime end){
+        List<Order> ordersToBeDelivered = orderRepository.findByOrderTimeBetween(start, end);
+        Map<String, Set<Order>> groupedOrders = ordersToBeDelivered.stream()
+                .collect(Collectors.groupingBy(order -> order.getUsername() + order.getAddress()));
+        return new HashSet<>(groupedOrders.values());
+    }
 }
