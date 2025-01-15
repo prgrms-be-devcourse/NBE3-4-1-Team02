106ca421c2173da618fbc301ca60a2e4fb3926b8
seng <kumjjick@icloud.com>
2025-01-15 15:27:18 +0900

    feat: 상품 엔티티 수정 및 패키지 구조 설정

---
+++ backend/src/main/java/com/example/nbe341team02/product/controller/ProductController.java
@@ -0,0 +1,4 @@
+package com.example.nbe341team02.product.controller;
+
+public class ProductController {
+}
---
+++ backend/src/main/java/com/example/nbe341team02/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.nbe341team02.product.repository;
+
+import com.example.nbe341team02.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ProductRepository extends JpaRepository<Product, Long> {
+}
