8ba6d264e7f0eee77c9a7c8c549fbbde17d98d26
이건희 <dnzp74@g.eulji.ac.kr>
2025-01-16 12:31:13 +0900

    test: 주문 성공 테스트

--- backend/src/test/java/com/example/nbe341team02/order/service/OrderServiceTest.java
+++ backend/src/test/java/com/example/nbe341team02/order/service/OrderServiceTest.java
@@ -87,5 +87,27 @@ public class OrderServiceTest {
         verify(orderProductRepository, never()).saveAll(anyList());
     }
 
+    @Test
+    @DisplayName("정상 테스트: 주문 성공")
+    public void testCreateOrder() {
+        // Given
+        Product product = new Product(1L, "Product 1", 1000, 3, true);
+        OrderCreateRequest request = new OrderCreateRequest(
+          "test@example.com",
+          "Test Address",
+          List.of(new OrderProductRequest(1L, 2))
+        );
+
+        when(productService.reduceStock(1L, 2)).thenReturn(product);
+
+        // When
+        Long orderId = orderService.createOrder(request);
+
+        // Then
+        verify(orderRepository).save(any(Order.class));
+        verify(orderProductRepository).saveAll(anyList());
+        verify(productService).reduceStock(1L, 2);
+    }
+
 }
 
