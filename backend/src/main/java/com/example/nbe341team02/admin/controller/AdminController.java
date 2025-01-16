9ccd9ce6a748c0a6b8f041ed625489fd3b1bf48b
jurio5 <sia5492@naver.com>
2025-01-16 23:44:17 +0900

    Fix : AdminController

--- backend/src/main/java/com/example/nbe341team02/admin/controller/AdminController.java
+++ backend/src/main/java/com/example/nbe341team02/admin/controller/AdminController.java
@@ -1,16 +1,10 @@
 package com.example.nbe341team02.admin.controller;
 
+import com.example.nbe341team02.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.nbe341team02.admin.service.AdminService;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,30 +18,8 @@ public class AdminController {
         return "admin-login";
     }
 
-    @PostMapping("/login")
-    public String login(@RequestParam(name = "username") String username,
-                        @RequestParam(name = "password") String password,
-                        HttpServletRequest request) {
-        if (adminService.authenticate(username, password)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("username", username);
-            return "redirect:/admin/dashboard";
-        } else {
-            return "redirect:/admin/login?error=true";
-        }
-    }
-
     @GetMapping("/dashboard")
     public String loadDashboard() {
         return "dashboard";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-        return "redirect:/admin/login";
     }
 }
--- backend/src/main/java/com/example/nbe341team02/admin/service/AdminService.java
+++ backend/src/main/java/com/example/nbe341team02/admin/service/AdminService.java
@@ -1,12 +1,12 @@
 package com.example.nbe341team02.admin.service;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
 import com.example.nbe341team02.admin.entity.Admin;
 import com.example.nbe341team02.admin.repository.AdminRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +15,14 @@ public class AdminService {
     private final AdminRepository adminRepository;
 
 
-    public boolean authenticate(String username, String password) {
-        Admin admin = adminRepository.findByAdminUsername(username).orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다."));
-        
-        return admin != null && admin.getAdminPassword().equals(password);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByAdminUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return User.builder()
+                .username(admin.getAdminUsername())
+                .password(admin.getAdminPassword())
+                .roles(admin.getAdminRole())
+                .build();
     }
 }
