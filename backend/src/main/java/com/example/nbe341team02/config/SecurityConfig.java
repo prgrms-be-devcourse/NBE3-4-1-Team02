c8f40cbfe4a7c666dff6331868575b3b26055f48
이건희 <dnzp74@g.eulji.ac.kr>
2025-01-16 09:13:30 +0900

    feat: H2 접속을 위한 SecurityConfig 설정 추가

--- backend/src/main/java/com/example/nbe341team02/config/SecurityConfig.java
+++ backend/src/main/java/com/example/nbe341team02/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.example.nbe341team02.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,5 +20,12 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
         ;
         return http.build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+          .requestMatchers(PathRequest.toH2Console());
     }
 }
