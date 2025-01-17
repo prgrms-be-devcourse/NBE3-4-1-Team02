7aa814383f5b4d6f5d01e182eed31d2051919ab8
seng <kumjjick@icloud.com>
2025-01-17 12:44:53 +0900

    feat: product 상품 추가 기능 Controller, Service 추가

--- backend/src/test/java/productTest/ProductServiceTest.java
+++ backend/src/test/java/productTest/ProductServiceTest.java
@@ -69,15 +69,6 @@ class ProductServiceTest {
                 .hasMessage(ErrorCode.PRODUCT_NOT_FOUND.getMessage());
     }
 
-//    @Test
-//    void 상품수정_감소(){
-//        //given
-//        Long id = 1L;
-//        ProductDTO reducedDTO = new ProductDTO(
-//                id,"reduced Product",
-//        )
-//    }
-
     @Test
     void 상품삭제_성공() {
         // given
