be18255c17eb90cd218014ad3ec5a316fc9a78c8
이건희 <dnzp74@g.eulji.ac.kr>
2025-01-17 14:55:16 +0900

    refactor: 주문 상태 한글로 반환하도록 enum 수정

--- backend/src/main/java/com/example/nbe341team02/domain/orders/enums/OrderStatus.java
+++ backend/src/main/java/com/example/nbe341team02/domain/orders/enums/OrderStatus.java
@@ -1,7 +1,16 @@
 package com.example.nbe341team02.domain.orders.enums;
 
 public enum OrderStatus {
-    COMPLETED,
-    CANCELLED;
+    COMPLETED("주문 완료"),
+    CANCELLED("주문 취소");
 
+    private final String korean;
+
+    OrderStatus(String korean) {
+        this.korean = korean;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
 }
