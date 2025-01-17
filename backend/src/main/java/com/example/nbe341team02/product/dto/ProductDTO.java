596dccc069d3578b25bd9f91da01b2014f20a36d
seng <kumjjick@icloud.com>
2025-01-17 14:15:35 +0900

    refactor: 상품 엔티티 및 DTO 변수명 변경

--- backend/src/main/java/com/example/nbe341team02/product/dto/ProductDTO.java
+++ backend/src/main/java/com/example/nbe341team02/product/dto/ProductDTO.java
@@ -1,5 +1,7 @@
 package com.example.nbe341team02.product.dto;
 
+
+import com.example.nbe341team02.product.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,4 +15,5 @@ public class ProductDTO {
     private int price;
     private int stock;
     private boolean status;
+
 }
