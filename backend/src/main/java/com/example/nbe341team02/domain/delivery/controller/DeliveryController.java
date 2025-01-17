f32afa1939dc8c1f4e69302864cfcc6fb9437e15
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-17 14:45:45 +0900

    refactor- 컨트롤러에 RequestMapping 추가

--- backend/src/main/java/com/example/nbe341team02/domain/delivery/controller/DeliveryController.java
+++ backend/src/main/java/com/example/nbe341team02/domain/delivery/controller/DeliveryController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,7 @@ public class DeliveryController {
     private final DeliveryScheduler deliveryScheduler;
 
     @PostMapping
+    @RequestMapping("/admin/policies/delivery-time")
     public ResponseEntity<Void> createNewDeliveryTimePolicy(
             @Valid @RequestBody DeliveryTimePolicyRegisterDto registerDto
             ){
