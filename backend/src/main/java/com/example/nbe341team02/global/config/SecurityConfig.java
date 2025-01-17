58ca06b1a8c38313a316ecc0f4da93955ddf520a
이건희 <dnzp74@g.eulji.ac.kr>
2025-01-17 14:56:02 +0900

    feat: 백엔드 서버와 프론트 서버 CORS 문제 해결을 위한 관련 설정 SecurityConfig에 추가

--- backend/src/main/java/com/example/nbe341team02/global/config/SecurityConfig.java
+++ backend/src/main/java/com/example/nbe341team02/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.nbe341team02.global.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -9,21 +11,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-        ;
+          .authorizeHttpRequests(authorize -> authorize
+            .anyRequest().permitAll()
+          )
+          .csrf(AbstractHttpConfigurer::disable)
+          .cors(withDefaults());
+
         return http.build();
     }
 
     @Bean
-    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
     public WebSecurityCustomizer configureH2ConsoleEnable() {
         return web -> web.ignoring()
           .requestMatchers(PathRequest.toH2Console());
