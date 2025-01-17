8f18efa262082204a343661038544db9b1c2a931
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-17 14:36:36 +0900

    feat- 배송 시간 정책 수정용 컨트롤러 매서드 추가 (내부적으로는 새로운 정책을 생성하므로 201로 반환)

--- backend/src/main/java/com/example/nbe341team02/domain/delivery/controller/DeliveryController.java
+++ backend/src/main/java/com/example/nbe341team02/domain/delivery/controller/DeliveryController.java
@@ -1,11 +1,25 @@
 package com.example.nbe341team02.domain.delivery.controller;
 
+import com.example.nbe341team02.domain.delivery.dto.DeliveryTimePolicyRegisterDto;
 import com.example.nbe341team02.domain.delivery.scheduler.DeliveryScheduler;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class DeliveryController {
     private final DeliveryScheduler deliveryScheduler;
+
+    @PostMapping
+    public ResponseEntity<Void> createNewDeliveryTimePolicy(
+            @Valid @RequestBody DeliveryTimePolicyRegisterDto registerDto
+            ){
+        deliveryScheduler.setDeliveryTime(registerDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
