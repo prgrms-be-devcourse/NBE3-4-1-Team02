2d0ad13c641ff7da49ad8bb34718544736908e59
seng <kumjjick@icloud.com>
2025-01-17 10:34:50 +0900

    chore: API 경로 설정 변경

--- backend/src/main/java/com/example/nbe341team02/product/controller/ProductController.java
+++ backend/src/main/java/com/example/nbe341team02/product/controller/ProductController.java
@@ -1,10 +1,10 @@
 package com.example.nbe341team02.product.controller;
 
-
 import com.example.nbe341team02.product.dto.ProductDTO;
 import com.example.nbe341team02.product.service.ProductService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j; // 여기에 추가
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/products")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
