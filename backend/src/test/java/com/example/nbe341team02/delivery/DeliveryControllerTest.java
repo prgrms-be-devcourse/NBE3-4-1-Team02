f1d5f6d3fee73cef527edcecb650b596830662d0
injunKang <149602262+InJunKangW@users.noreply.github.com>
2025-01-17 12:35:49 +0900

    test- 배송 컨트롤러 테스트 클래스 추가 (TDD-R단계)

---
+++ backend/src/test/java/com/example/nbe341team02/delivery/DeliveryControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.nbe341team02.delivery;
+
+import com.example.nbe341team02.domain.delivery.scheduler.DeliveryScheduler;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(DeliveryController.class)
+public class DeliveryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private DeliveryScheduler deliveryScheduler;
+
+    @Test
+    void contextLoads() {}
+}
