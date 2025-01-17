acae72545563d24ca1b8e289ed88b07a86052269
jurio5 <sia5492@naver.com>
2025-01-18 01:34:32 +0900

    Add : JwtAuthenticationFilter

---
+++ backend/src/main/java/com/example/nbe341team02/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.example.nbe341team02.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // HTTP 중복 요청 방지
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request); // HTTP 요청 헤더에서 JWT 토큰 추출 작업
+
+        if (token != null && jwtTokenProvider.validateToken(token)) { // 토큰 O , 유효성 검사 O
+            Authentication auth = jwtTokenProvider.getAuthentication(token); // 토큰에서 사용자 정보 추출 후 Authentication 객체를 반환
+            SecurityContextHolder.getContext().setAuthentication(auth); // 인증 된 사용자 정보를 SecurityContext 에 저장
+        }
+
+        filterChain.doFilter(request, response); // 다음 필터로 넘어가기
+    }
+}
