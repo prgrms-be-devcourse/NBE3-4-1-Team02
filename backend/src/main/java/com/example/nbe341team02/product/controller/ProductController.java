23f0223d9c03e5dea3d5ad7046ec45adee694fbe
notmybussiness <cuisine97@naver.com>
2025-01-17 14:30:31 +0900

    feat - 수정 삭제 기능 추가 테스트 x

--- backend/src/main/java/com/example/nbe341team02/product/controller/ProductController.java
+++ backend/src/main/java/com/example/nbe341team02/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.nbe341team02.product.controller;
 
 import com.example.nbe341team02.product.dto.ProductDTO;
+import com.example.nbe341team02.product.repository.ProductRepository;
 import com.example.nbe341team02.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j; // 여기에 추가
@@ -28,5 +29,19 @@ public class ProductController {
     public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO){
         ProductDTO createdProduct = productService.addProduct(productDTO);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    //상품 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO,
+                                                    @PathVariable("id") Long id){
+        ProductDTO updatedProduct = productService.updateProduct(id,productDTO);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
