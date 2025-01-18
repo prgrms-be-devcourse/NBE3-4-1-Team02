f01e4eeef2092e3f82502e60517496379ab2acd5
hellonaeunkim <hellonaeunkim@gmail.com>
2025-01-18 16:10:27 +0900

    refactor : JWT 관련 Exception 추가

--- backend/src/main/java/com/example/nbe341team02/config/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/nbe341team02/config/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.nbe341team02.config.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(IllegalArgumentException.class) // login 에서 비밀번호가 일치하지 않거나 사용자를 찾지 못할 시 예외
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
@@ -16,5 +19,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UsernameNotFoundException.class) // loadUserByUsername 에서 사용자를 찾지 못할 시 예외
     public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    // 토큰 만료 시 예외
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰이 만료되었습니다.");
+    }
+
+    // JWT 형식이 올바르지 않을 때  예외
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<String> handleMalformedJwtException(MalformedJwtException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("잘못된 JWT 서명입니다.");
+    }
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<String> handleSignatureException(SignatureException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("JWT 서명이 유효하지 않습니다.");
     }
 }
