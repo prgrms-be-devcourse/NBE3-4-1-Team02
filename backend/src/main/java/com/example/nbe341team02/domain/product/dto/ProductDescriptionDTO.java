9a3a3b7a5a6235a977fe1bc71ea7188405361493
seng <kumjjick@icloud.com>
2025-01-19 16:19:15 +0900

    feat:ProductDTO, ProductDescriptionDTO 나누어서 생성

---
+++ backend/src/main/java/com/example/nbe341team02/domain/product/dto/ProductDescriptionDTO.java
@@ -0,0 +1,10 @@
+package com.example.nbe341team02.domain.product.dto;
+
+public class ProductDescriptionDTO {
+    private Long id;
+    private String name;
+    private int price;
+    private int stock;
+    private boolean status;
+    private String description;
+}
