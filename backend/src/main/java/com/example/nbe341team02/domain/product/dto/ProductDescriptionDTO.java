bb5ddcf45cd4f4acd1683a0001f01fe577d14b92
seng <kumjjick@icloud.com>
2025-01-19 16:19:15 +0900

    feat:ProductDTO, ProductDescriptionDTO 나누어서 생성

--- backend/src/main/java/com/example/nbe341team02/domain/product/dto/ProductDescriptionDTO.java
+++ backend/src/main/java/com/example/nbe341team02/domain/product/dto/ProductDescriptionDTO.java
@@ -1,17 +1,10 @@
 package com.example.nbe341team02.domain.product.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class ProductDTO {
+public class ProductDescriptionDTO {
     private Long id;
     private String name;
     private int price;
     private int stock;
     private boolean status;
-
+    private String description;
 }
