71ed7f66c1e3d88d9bcfb54ca7c469964f4963a4
jurio5 <sia5492@naver.com>
2025-01-19 19:44:02 +0900

    Fix SecurityConfig

--- backend/src/main/java/com/example/nbe341team02/config/SecurityConfig.java
+++ backend/src/main/java/com/example/nbe341team02/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.nbe341team02.config;
 
-import com.example.nbe341team02.admin.service.AdminService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
