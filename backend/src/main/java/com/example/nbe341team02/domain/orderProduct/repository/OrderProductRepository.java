818d13ee4ada10e2e3f58005aaabd1cebf943837
jurio5 <sia5492@naver.com>
2025-01-19 19:09:28 +0900

    feat: Admin 관련 코드 분리

--- backend/src/main/java/com/example/nbe341team02/domain/orderProduct/repository/OrderProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.nbe341team02.domain.orderProduct.repository;
-
-import com.example.nbe341team02.domain.orderProduct.entity.OrderProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
-
-}
--- backend/src/main/java/com/example/nbe341team02/domain/orders/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.nbe341team02.domain.orders.repository;//package com.example.nbe341team02.domain.order.repository;
-
-import com.example.nbe341team02.domain.orders.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
