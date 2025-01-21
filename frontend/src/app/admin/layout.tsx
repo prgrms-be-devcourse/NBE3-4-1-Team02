2f7467281465de74a6fcf853a1c659e0cadc2d8d
notmybussiness <cuisine97@naver.com>
2025-01-21 12:05:35 +0900

    상품관리페이지 생성

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,11 @@
+export default function ProductsLayout({
+                                           children,
+                                       }: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            {children}
+        </div>
+    )
+}
